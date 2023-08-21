9722b0e09a3db57a4f858e70165447543c473ced
Davi Vasconcellos <davijvasconcellos@gmail.com>
2023-08-21 08:27:06 -0300

    Exercícios de Programação com decisão em Python

--- lista042/questao10.py
+++ lista042/questao10.py
@@ -8,15 +8,15 @@ entre = 3,0 e 6,9 = prova final
 a partir de 7 aprovado
 """
 
+nome = input("Informe o seu nome: ")
 n1 = float(input("Informe a sua nota 1: "))
 n2 = float(input("Informe a sua nota 2: "))
 
 media = (n1 + n2) / 2
 
-if (media < 3):
-    print(f"Você está reprovado, sua média é {media}.")
-
-elif (media >= 3 and media == 6.9):
-    print(f"Você está de prova final, sua média é {media}.")
+if (media < 3.0):
+    print(f"Aluno {nome} você está reprovado, sua média é {media:.1f}.")
+elif (media <= 6.9):
+    print(f"Aluno {nome} você está de prova final, sua média é {media:.1f}.")
 else:
-    print(f"Você está aprovado, sua média é {media}.")
+    print(f"Aluno {nome} você está aprovado, sua média é {media:.1f}.")
